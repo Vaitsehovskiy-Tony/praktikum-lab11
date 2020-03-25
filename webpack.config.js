9020fc351fd32fea30285cfdf730a2c1d93222a5
Anton Vaitsehovskiy <antonvaits@gmail.com>
2020-03-25 20:40:00 +0300

    Setup loadash and Node_Env

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // добавили плагин
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// подключаем плагин
+const isDev = process.env.NODE_ENV === 'development';
+// создаем переменную для development-сборки
+
 
 module.exports = {
   entry: { main: './src/script/index.js' },
@@ -22,9 +27,13 @@ module.exports = {
       },
 
       {
-        test: /\.css$/,
-        use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] // добавили минификацию CSS
-      },
+        test: /\.css$/i,
+        use: [
+          (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+          'css-loader', 
+          'postcss-loader'
+              ]
+},
       
       {
         test: /\.(png|jpg|gif|ico|svg)$/,
@@ -47,11 +56,26 @@ module.exports = {
     new MiniCssExtractPlugin({ 
       filename: 'style.[contenthash].css',
     }),
+
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+              preset: ['default'],
+      },
+      canPrint: true
+    }),
+
     new HtmlWebpackPlugin({
       inject: false,
       template: './src/index.html',
       filename: 'index.html'
     }),
-    new WebpackMd5Hash()
+
+    new WebpackMd5Hash(),
+
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
   ]
 };
